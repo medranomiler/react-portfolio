fc431b237ca521b3fd88f6ff288c51b0fd1f173a
Medranomiler1 <medranomiler@gmail.com>
2023-03-03 11:10:15 -0600

    added headshot and about me content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import "./assets/styles/header.css"
 
-// const styles = {
-//     header: {
-//         height: '200px',
-//         backgroundImage: "url('https://t3.ftcdn.net/jpg/04/56/11/06/360_F_456110688_0RbzwTy8UVyZwtObuxr8lb3XRr28R24X.jpg')"
-//     }
-// }
 
 function Header(){
     return(
         <div className="header">
             <header>
-                <h1>
-                    Header
-                </h1>
             </header>
         </div>
 )}
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './assets/styles/project.css'
+import headshot from "./assets/images/headshot.png"
 function Project(){
     
     return(
         <div className="project">
-            <main>
-                <h3>
-                    Project
-                </h3>
+            <main className="content">
+                <p className="heading">
+                    About Me
+                </p>
+                <img
+                src={headshot}
+                />
+                <p className="paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
             </main>
         </div>
 )}
