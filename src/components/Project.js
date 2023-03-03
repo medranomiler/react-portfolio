28f38224dc87977e5229432aea7c321c2732ce8d
Medranomiler1 <medranomiler@gmail.com>
2023-03-03 16:17:03 -0600

    added the conditional rendering starting code

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import './assets/styles/project.css'
 import headshot from "./assets/images/headshot.png"
+import {useState} from "react"
+
+
 function Project(){
     
+const [aboutMe, setAboutMe] = useState(false)
+const [portfolio, setPortfolio] = useState(false)
+const [contact, setContact] =useState(true)
+const [resume, setResume] = useState(false)
+
+
+// function AboutMeHandler(e){
+//     e.preventDefault()
+//     // portfolio
+//     // contact
+//     // resume
+//     setAboutMe = true
+//     return
+// }
+// function PortfolioHandler(e){
+//     e.preventDefault()
+//     aboutMe
+//     contact
+//     resume
+//     setPortfolio = true
+//     return
+// }
+
+    if(aboutMe && !portfolio && !contact && !resume){
     return(
         <div className="project">
             <main className="content">
@@ -17,7 +44,39 @@ function Project(){
                 </blockquote>
             </main>
         </div>
-)}
+)
+}else if(!aboutMe && portfolio && !contact && !resume){
+    return(
+        <div className="project">
+            <main className="content">
+            <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+                    Portfolio
+            </h1>
+            </main>
+        </div>
+    )
+}else if(!aboutMe && !portfolio && contact && !resume){
+    return(
+        <div className="project">
+            <main className="content">
+            <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+                    Contact
+            </h1>
+            </main>
+        </div>
+    )
+}else if(!aboutMe && !portfolio && contact && resume){
+    return(
+        <div className="project">
+            <main className="content">
+            <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+                    Resume
+            </h1>
+            </main>
+        </div>
+    )
+}
+}
 
 
 export default Project;
