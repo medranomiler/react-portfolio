1771a5ab21131739868eb0dba8c06f27597abb93
Medranomiler1 <medranomiler@gmail.com>
2023-03-04 21:55:48 -0600

    added onClick attr to nav logo

--- src/App.js
+++ src/App.js
@@ -42,7 +42,8 @@ const [content, setContent] = useState(false)
     <div class="body">
       <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
-    <div class="flex items-center">
+    <div class="flex items-center cursor-pointer"
+    onClick={aboutMeHandler}>
         <img 
         src={Logo}
         class="h-6 sm:h-6" alt="Logo" />
