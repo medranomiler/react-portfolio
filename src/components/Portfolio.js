c8cfa2cdb658744b7b7de1a9777992abdbf2e6a6
Medranomiler1 <medranomiler@gmail.com>
2023-03-05 22:28:27 -0600

    decreased photo font size

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -21,7 +21,7 @@ function Portfolio(){
 <a href="https://space-defence.herokuapp.com/">
 <img class="rounded-lg" src={Img1}/>
 </a>
-<figcaption class="absolute px-4 text-2xl text-white bottom-6">
+<figcaption class="absolute px-4 text-lg text-white bottom-6">
       <p>Space Defence</p>
   </figcaption>
 </figure>
@@ -32,7 +32,7 @@ function Portfolio(){
 <a href="https://my-notes.herokuapp.com/">
 <img class="rounded-lg" src={Img2}/>
 </a>
-<figcaption class="absolute px-4 text-2xl text-white bottom-6">
+<figcaption class="absolute px-4 text-lg text-white bottom-6">
       <p>My Notes</p>
   </figcaption>
 </figure>
@@ -43,7 +43,7 @@ function Portfolio(){
 <a href="https://not-just-another-text-editor.herokuapp.com/">
 <img class="rounded-lg" src={Img3}/>
 </a>
-<figcaption class="absolute px-4 text-2xl text-white bottom-6">
+<figcaption class="absolute px-4 text-lg text-white bottom-6">
       <p>Just Another Text Editor</p>
   </figcaption>
 </figure>
@@ -54,7 +54,7 @@ function Portfolio(){
 <a href="https://medranomiler.github.io/weather-dashboard/">
 <img class="rounded-lg" src={Img4} />
 </a>
-<figcaption class="absolute px-4 text-2xl text-gray bottom-6">
+<figcaption class="absolute px-4 text-lg text-gray bottom-6">
       <p>Weather Dashboard</p>
   </figcaption>
 </figure>
@@ -65,7 +65,7 @@ function Portfolio(){
 <a href="https://the-tech-blog.herokuapp.com/">
 <img class="rounded-lg" src={Img5}/>
 </a>
-<figcaption class="absolute px-4 text-2xl text-white bottom-6">
+<figcaption class="absolute px-4 text-lg text-white bottom-6">
       <p>The Tech Blog</p>
   </figcaption>
 </figure>
@@ -76,7 +76,7 @@ function Portfolio(){
 <a href="https://github.com/medranomiler/readme-generator">    
 <img class="rounded-lg" src={Img6}/>
 </a>
-<figcaption class="absolute px-4 text-2xl text-white bottom-6">
+<figcaption class="absolute px-4 text-lg text-white bottom-6">
       <p>ReadMe Generator</p>
   </figcaption>
 </figure>
